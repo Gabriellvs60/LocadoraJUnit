d6de32d5b436c56e324bfa7b86444bb6e41f3b0b
Gabriel <gabriellvs60@gmail.com>
2018-09-24 10:30:49 -0300

    Testes Em FilmeTest
    Iniciando testes em "FilmeTest"

--- src/test/java/local/model/FilmeTest.java
+++ src/test/java/local/model/FilmeTest.java
@@ -7,9 +7,21 @@ import javax.validation.Valid;
 
 public class FilmeTest {
     //TODO: Nome deve possuir entre 2 e 99 caracteres (inclusive)
+     @Test
+    public void nomeEntreIntervaloDeCaracteres() {
+    //intervalo aceitável é de 2 a 99
+    
+    }
+
     //TODO: Estoque não pode ser negativo, e o máximo é 99 (inclusive)
+    @Test
+    public void EstoqueNaoPodeSairDosLimites() {
+
+    }
+   
     //TODO: O preço de Locacao deverá ser um número positivo entre R$ 1,00 e R$ 9,99 (inclusive)
+    @Test
+    public void PrecoNaoPodeSairDosLimites() {
 
-
-
+    }
 }
