dac0c7c010e7ab17ada08bb6a52176f6c882ff1c
Gabriel <gabriellvs60@gmail.com>
2018-09-24 10:36:28 -0300

    Testes em FilmeTest
    Nomeando Testes da "FilmeTest"

--- src/test/java/local/model/FilmeTest.java
+++ src/test/java/local/model/FilmeTest.java
@@ -7,21 +7,38 @@ import javax.validation.Valid;
 
 public class FilmeTest {
     //TODO: Nome deve possuir entre 2 e 99 caracteres (inclusive)
+    
      @Test
-    public void nomeEntreIntervaloDeCaracteres() {
+    public void nomeIntervaloMinimoDeCaracteres() {
+    //intervalo aceitável é de 2 a 99
+    
+    }
+    
+      @Test
+    public void nomeIntervaloMaximoDeCaracteres() {
     //intervalo aceitável é de 2 a 99
     
     }
 
     //TODO: Estoque não pode ser negativo, e o máximo é 99 (inclusive)
     @Test
-    public void EstoqueNaoPodeSairDosLimites() {
+    public void EstoqueNaoPodeNegativo() {
+
+    }
+    
+      @Test
+    public void EstoqueNaoPodePassarLimites() {
 
     }
    
     //TODO: O preço de Locacao deverá ser um número positivo entre R$ 1,00 e R$ 9,99 (inclusive)
     @Test
-    public void PrecoNaoPodeSairDosLimites() {
+    public void PrecoMinimoFilme() {
+
+    }
+    
+     @Test
+    public void PrecoMaximoFilme() {
 
     }
 }
